92a14ee13489b6abea84be098b52f578c5e91e87
firmankagami <firmanda.nugroho@yahoo.com>
2017-12-12 20:49:17 +0700

    ViewPager fix, toolbar sudah jadi.

---
+++ app/src/main/java/reminderobat/android/kelompok5/papbc/com/reminderobat/FragmentActivity.java
@@ -0,0 +1,29 @@
+package reminderobat.android.kelompok5.papbc.com.reminderobat;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class FragmentActivity extends AppCompatActivity {
+    TabLayout tabs;
+    ViewPager pager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        Toolbar mToolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(new TabAdapter(getSupportFragmentManager()));
+        tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.setTabTextColors(getResources().getColor(R.color.colorPrimary),
+                getResources().getColor(R.color.cian700));
+        tabs.setSelectedTabIndicatorColor(getResources().getColor(R.color.colorPrimaryDark));
+        tabs.setupWithViewPager(pager);
+        tabs.setTabGravity(TabLayout.GRAVITY_FILL);
+    }
+}
---
+++ app/src/main/java/reminderobat/android/kelompok5/papbc/com/reminderobat/TabAdapter.java
@@ -0,0 +1,48 @@
+package reminderobat.android.kelompok5.papbc.com.reminderobat;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Firmanda on 12/12/2017.
+ */
+
+public class TabAdapter extends FragmentPagerAdapter {
+    String[] title = new String[]{
+            "Daftar", "Reminder"
+    };
+
+    public TabAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    //method ini yang akan memanipulasi penampilan Fragment dilayar
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch (position) {
+            case 0:
+                fragment = new DaftarFragment();
+                break;
+            case 1:
+                fragment = new ReminderFragment();
+                break;
+            default:
+                fragment = null;
+                break;
+        }
+
+        return fragment;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return title[position];
+    }
+
+    @Override
+    public int getCount() {
+        return title.length;
+    }
+}
