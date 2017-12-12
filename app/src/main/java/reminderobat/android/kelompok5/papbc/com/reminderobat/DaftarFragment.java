08f13deefb882126dc75351f7356ac40a1229c42
firmankagami <firmanda.nugroho@yahoo.com>
2017-12-12 22:07:10 +0700

    RecycleView Daftar FIX!!!!! COK!

--- app/src/main/java/reminderobat/android/kelompok5/papbc/com/reminderobat/DaftarFragment.java
+++ app/src/main/java/reminderobat/android/kelompok5/papbc/com/reminderobat/DaftarFragment.java
@@ -2,10 +2,16 @@ package reminderobat.android.kelompok5.papbc.com.reminderobat;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,6 +19,9 @@ import android.view.ViewGroup;
  */
 public class DaftarFragment extends Fragment {
 
+    private List<DaftarModel> daftarModelList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private DaftarRecycleAdapter adapter;
 
     public DaftarFragment() {
         // Required empty public constructor
@@ -23,7 +32,20 @@ public class DaftarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_daftar, container, false);
+       View v= inflater.inflate(R.layout.fragment_daftar, container, false);
+        FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.fab_admin);
+
+        recyclerView = (RecyclerView) v.findViewById(R.id.recycle_view_daftar);
+        adapter = new DaftarRecycleAdapter(daftarModelList, getContext());
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setAdapter(adapter);
+        daftarModelList.add(new DaftarModel("Obat 1","Keterangan 1"));
+        daftarModelList.add(new DaftarModel("Obat 1","Keterangan 2"));
+
+
+        adapter.notifyDataSetChanged();
+       return v;
     }
 
 }
---
+++ app/src/main/java/reminderobat/android/kelompok5/papbc/com/reminderobat/DaftarModel.java
@@ -0,0 +1,22 @@
+package reminderobat.android.kelompok5.papbc.com.reminderobat;
+
+/**
+ * Created by Firmanda on 12/12/2017.
+ */
+
+public class DaftarModel {
+    public DaftarModel(String namaObat, String keterangan) {
+        this.namaObat = namaObat;
+        this.keterangan = keterangan;
+    }
+
+    public String getNamaObat() {
+        return namaObat;
+    }
+
+    public String getKeterangan() {
+        return keterangan;
+    }
+
+    String namaObat,keterangan;
+}
