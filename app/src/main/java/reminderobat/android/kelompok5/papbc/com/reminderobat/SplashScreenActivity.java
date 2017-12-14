43b36e4c469e31532fd0858b755a8064ae2b519a
Galang Ptrm <galangptrm@student.ub.ac.id>
2017-12-14 21:28:58 +0700

    pull kuy

--- app/src/main/java/reminderobat/android/kelompok5/papbc/com/reminderobat/SplashScreenActivity.java
+++ app/src/main/java/reminderobat/android/kelompok5/papbc/com/reminderobat/SplashScreenActivity.java
@@ -3,11 +3,17 @@ package reminderobat.android.kelompok5.papbc.com.reminderobat;
 import android.app.Activity;
 import android.os.Bundle;
 
+import java.util.Timer;
+
 public class SplashScreenActivity extends Activity {
+    Thread mthWait;
+    Timer mtmTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
     }
 }
